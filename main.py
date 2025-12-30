d02506c05d65d973e269599aeb4df5eb213585f0
Chengjie Xu (from Dev Box) <7625061+eugene2candy@users.noreply.github.com>
2025-12-30 14:46:52 +0800

    feat: integrate Azure OpenAI API

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from windows_use.llms.google import ChatGoogle
 from windows_use.llms.anthropic import ChatAnthropic
 from windows_use.llms.ollama import ChatOllama
 from windows_use.llms.mistral import ChatMistral
+from windows_use.llms.azure_openai import ChatAzureOpenAI
 from windows_use.agent import Agent, Browser
 from dotenv import load_dotenv
 import os
@@ -14,6 +15,14 @@ def main():
     # llm=ChatGoogle(model="gemini-3-flash-preview",thinking_budget=0, api_key=api_key, temperature=0.7)
     # llm=ChatAnthropic(model="claude-sonnet-4-5", api_key=api_key, temperature=0.7,max_tokens=1000)
     # llm=ChatOllama(model="qwen3-vl:235b-cloud",temperature=0.2)
+    # llm=ChatAzureOpenAI(
+    #     endpoint=os.getenv("AOAI_ENDPOINT"),
+    #     deployment_name=os.getenv("AOAI_DEPLOYMENT_NAME"),
+    #     api_key=os.getenv("AOAI_API_KEY"),
+    #     model=os.getenv("AOAI_MODEL"),
+    #     api_version=os.getenv("AOAI_API_VERSION", "2025-01-01-preview"),
+    #     temperature=0.7
+    # )
     agent = Agent(llm=llm, browser=Browser.EDGE, use_vision=False, auto_minimize=False)
     agent.print_response(query=input("Enter a query: "))
 
